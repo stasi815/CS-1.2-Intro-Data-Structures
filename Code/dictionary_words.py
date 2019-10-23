b32a60697567422776a50dd7e39d1e64f5d210d6
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-23 12:41:23 -0700

    produces random sentences

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,13 +1,46 @@
 import random
+import sys
 
 def get_random_word():
-    with open('dict_words.txt', 'r') as f:
-        words_list = f.readlines()
-    random_word = random.choice(words_list)
+    with open('/usr/share/dict/words', 'r') as f:
+        words_list = f.read().splitlines() # read text file
+    random_word = random.choice(words_list) # get random word from file
     return random_word
 
-# def sentence_build():
+def sentence_build(params):
+    list_one = [] # list of randomly chosen words from file
+    list_two = []
+
+    count = 0
+
+    while count < int(params):
+        list_one.append(get_random_word())
+        count += 1
+
+    print(" ".join(list_one) + ".")
+    # print(list_one)
+
+
 
 
 if __name__ == "__main__":
-    print(get_random_word())
+    params = sys.argv[1]
+    sentence_build(params)
+
+# def rearrange_words(params):
+#     word_list = []
+#     word_list = params # establishes that input will be from terminal
+#     random_list = [] # new list made from random picks out of word_list
+
+#     while len(random_list) < len(word_list): # stop doing this function after all words entered have been randomly returned
+#         random_word = random.choice(params) #choose random word from params
+#         if random_word not in random_list: # don't repeat a word that has already been chosen
+#             random_list.append(random_word) # add random words to new list
+
+
+#     print(" ".join(random_list)) # print the list as a concatinated string with spaces in between
+
+
+# if __name__ == "__main__":
+#     params = sys.argv[1:]
+#     rearrange_words(params)
