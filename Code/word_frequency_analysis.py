16fbfe0563a0d6ca9913152f08fee9089aca9d8a
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-10-28 13:56:18 -0700

    dict_histogram function

--- Code/word_frequency_analysis.py
+++ Code/word_frequency_analysis.py
@@ -1,10 +1,25 @@
 def load_text():
     with open('/Users/makeschoolloaner/dev/CS1.2/CS-1.2-Intro-Data-Structures/Code/parks_and_rec_test.txt', 'r') as f:
         doc_text = f.read().splitlines()
-        source_text = (" ".join(doc_text)).split()
+        source_text = ((" ".join(doc_text)).lower()).split()
     return source_text
 
-# def histogram(source_text):
+def dict_histogram(source_text):
+
+    histogram = {}
+
+    for word in source_text:
+        if word not in histogram:
+            histogram[word] = 1
+        else:
+            histogram[word] += 1
+
+    return histogram
+
+
+# def tuple_histogram(source_text):
+
+# def list_histogram(source_text):
 
 # def unique_words(histogram):
 
@@ -13,4 +28,4 @@ def load_text():
 
 if __name__ == "__main__":
     source_text = load_text()
-    print(source_text)
+    print(dict_histogram(source_text))
