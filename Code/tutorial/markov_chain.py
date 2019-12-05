ed2442a29d7acd19f1ddb741ca36684ad92fa76e
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-12-04 22:46:14 -0800

    second order histogram progress pre Dictogram

--- Code/tutorial/markov_chain.py
+++ Code/tutorial/markov_chain.py
@@ -1,6 +1,6 @@
-from Code.tutorial.dictogram import Dictogram, print_histogram
-import Code.tutorial.histogram_sentence
-from Code.tutorial.word_frequency_analysis import load_text
+from dictogram import Dictogram, print_histogram
+import histogram_sentence
+from word_frequency_analysis import load_text
 import random
 
 def markov_histo(word_list):
@@ -16,6 +16,34 @@ def markov_histo(word_list):
             # STINE TESTING Thank you Aucoeur!!
 
         histo[key_word].append(next_key_word)
+
+    value_list = histo.items()
+    for key, value in value_list:
+        histo[key] = Dictogram(value)
+
+    return histo
+
+def ho_markov_histo(word_list):
+    '''Creates histogram that represents markov chain for each word in a list.'''
+    histo = {}
+    for i in range(len(word_list)-1):
+        key_word = word_list[i]
+        next_key_word = word_list[i + 1]
+        new_key = (key_word, next_key_word)
+
+
+
+        print(i, new_key)
+
+        if i+2 < len(word_list):
+            next_next_key = word_list[i + 2]
+
+            if new_key not in histo.keys():
+                key_histo = []
+                histo[new_key] = key_histo
+            # STINE TESTING Thank you Aucoeur!!
+            histo[new_key].append(next_next_key)
+
 
     value_list = histo.items()
     for key, value in value_list:
@@ -50,9 +78,9 @@ def main():
     #     print_histogram(woodchuck_text.split())
     filename = "corpus_texts/one_fish_two.txt"
     word_list = load_text(filename)
-    markov_histos = markov_histo(word_list)
-    print(markov_histos)
-    print(markov_run(markov_histos))
+    markov_histos = ho_markov_histo(word_list)
+    # print(markov_histos)
+    # print(markov_run(markov_histos))
 
 
 if __name__ == '__main__':
