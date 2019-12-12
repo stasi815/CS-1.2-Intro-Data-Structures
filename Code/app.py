8e619bf19558d3db35a28971219f690e2bbef950
stasi815 <anastasia.gallardo@students.makeschool.com>
2019-12-11 19:58:30 -0800

    bhagavad-gita added

--- Code/app.py
+++ Code/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 from random import randint, uniform
 from word_frequency_analysis import load_text, dict_histogram
-# import Code.tutorial.histogram_sentence
-from markov_chain import ho_markov_histo, markov_run
+from markov_chain import second_order_markov_histo, markov_run
 
 
 
@@ -11,22 +10,25 @@ app = Flask(__name__)
 @app.route('/')
 def rand_sentence_generator():
     """Returns a random sentence based on weighted word probability."""
-    filename = 'corpus_texts/parks_and_rec.txt'
+    filename = 'corpus_texts/bhagavad_gita.txt'
     word_list = load_text(filename)
-    markov_histos = ho_markov_histo(word_list)
+    markov_histos = second_order_markov_histo(word_list)
 
-    word_list = []
-    count = 0
-    while count < randint(5, 15):
-        random_word = markov_run(markov_histos)
+    sentence = markov_run(markov_histos)
 
-        word_list.append(random_word)
-        count += 1
+    # word_list = []
+    # count = 0
+    # while count < randint(5, 15):
+    #     random_word = markov_run(markov_histos)
+
+    #     word_list.append(random_word)
+    #     count += 1
 
 
-    random_sentence = " ".join(word_list).capitalize() + "."
+
+
+    random_sentence = " ".join(sentence).capitalize() + "."
     return render_template("index.html", sentence=random_sentence)
-    # return f"{ random_sentence }"
 
 
 if __name__ == '__main__':
--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -1,29 +1,31 @@
+# TUTORIAL
 from dictogram import Dictogram, print_histogram
+from queue import Queue
 import histogram_sentence
 from word_frequency_analysis import load_text
 import random
 
-def markov_histo(word_list):
-    '''Creates histogram that represents markov chain for each word in a list.'''
-    histo = {}
-    for i in range(len(word_list)-1):
-        key_word = word_list[i]
-        next_key_word = word_list[i + 1]
+# def markov_histo(word_list):
+#     '''Creates histogram that represents markov chain for each word in a list.'''
+#     histo = {}
+#     for i in range(len(word_list)-1):
+#         key_word = word_list[i]
+#         next_key_word = word_list[i + 1]
 
-        if key_word not in histo.keys():
-            key_histo = []
-            histo[key_word] = key_histo
-            # STINE TESTING Thank you Aucoeur!!
+#         if key_word not in histo.keys():
+#             key_histo = []
+#             histo[key_word] = key_histo
+#             # STINE TESTING Thank you Aucoeur!!
 
-        histo[key_word].append(next_key_word)
+#         histo[key_word].append(next_key_word)
 
-    value_list = histo.items()
-    for key, value in value_list:
-        histo[key] = Dictogram(value)
+#     value_list = histo.items()
+#     for key, value in value_list:
+#         histo[key] = Dictogram(value)
 
-    return histo
+#     return histo
 
-def ho_markov_histo(word_list):
+def second_order_markov_histo(word_list):
     '''Creates histogram that represents markov chain for each word in a list.'''
     histo = {}
     for i in range(len(word_list)-1):
@@ -47,35 +49,43 @@ def ho_markov_histo(word_list):
 
     return histo
 
-def markov_run(markov_histos):
-    keys_list = markov_histos.keys()
-    first_word = random.choice(list(keys_list))
-    markov_key = markov_histos.get(first_word)
-    return(markov_key.sample())
+def markov_run(markov_histos, steps=10):
+    keys_list = markov_histos.keys() # makes list of tupled words
+    first_tuple = random.choice(list(keys_list)) # picks random tuple of words to begin sentence
+    markov_key = markov_histos.get(first_tuple) # dictogram values for selected tuple
+    next_word = markov_key.sample() # picks weighted word from histogram of keys
+    first_word = first_tuple[0]
+    second_word = first_tuple[1]
+
+    sentence = []
+    sentence.append(first_word)
+    sentence.append(second_word)
+
+    next_tuple = (second_word, next_word)
+
+    sentence.append(next_word)
+
+
+    i = 2
+    while i != steps:
+        dict_sample = markov_histos.get(next_tuple)
+        next_word2 = dict_sample.sample()
+        sentence.append(next_word2)
+        next_tuple = (next_tuple[1], next_word2)
+        i += 1
+    return sentence
+
 
 
 def main():
-    # import sys
-    # arguments = sys.argv[1:]  # Exclude script name in first argument
-    # if len(arguments) >= 1:
-    #     # Test histogram on given arguments
-    #     print_histogram(arguments)
-    # else:
-    #     # Test histogram on letters in a word
-    #     word = 'abracadabra'
-    #     print_histogram(list(word))
-    #     # Test histogram on words in a classic book title
-    #     fish_text = 'one fish two fish red fish blue fish'
-    #     print_histogram(fish_text.split())
-    #     # Test histogram on words in a long repetitive sentence
-    #     woodchuck_text = ('how much wood would a wood chuck chuck'
-    #                       ' if a wood chuck could chuck wood')
-    #     print_histogram(woodchuck_text.split())
-    filename = "corpus_texts/one_fish_two.txt"
+    # filename = "corpus_texts/one_fish_two.txt"
+    # filename = "corpus_texts/parks_and_rec.txt"
+    filename = "corpus_texts/bhagavad_gita.txt"
     word_list = load_text(filename)
-    markov_histos = ho_markov_histo(word_list)
-    print(markov_histos)
-    print(markov_run(markov_histos))
+    markov_histos = second_order_markov_histo(word_list)
+    word_one = markov_run(markov_histos)
+    print(word_one)
+
 
 
 if __name__ == '__main__':
